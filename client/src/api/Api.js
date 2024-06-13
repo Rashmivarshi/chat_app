b008fefd6f4be3bae2302f6c4910968b0bfba927
Rashmivarshi <rashmimaniampare@gmail.com>
2024-06-13 12:25:23 +0530

    commit

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://localhost:5000",
 });
 
 export const fetchdata = async () => await API.get("/api/chat");
